fd4194ffda103b53dfd0d906acaac11c66b006ea
anor32 <burgchiken@gmail.com>
2025-11-26 22:41:51 +0300

    parcer сайта создан

---
+++ parcers.py
@@ -0,0 +1,19 @@
+import re
+
+import bs4
+import httpx
+from bs4 import BeautifulSoup
+
+
+class NewsParser:
+    url = 'https://www.warhammer-community.com/en-gb/'
+    def parce(self):
+        source = httpx.get(url=self.url)
+        soup = BeautifulSoup(source.text,'lxml')
+        news_list = soup.find('ul',class_='row flex flex-wrap gap-y-10 md:gap-y-20 xl:gap-y-30')
+        s = news_list.find_all('li',class_='column w-full md:!w-6/12 lg:!w-4/12 xl:!w-3/12')
+        news = s[0]
+        link = news.find('a',class_='btn-cover mb-15 md:mb-20 block link-underline link-underline--light')
+        bad_url = self.url+ link.get('href')
+        good_url = re.sub(r'en-gb//en-gb/', 'en-gb/', bad_url)
+        return good_url
