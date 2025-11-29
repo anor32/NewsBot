48cbc2fef3df06151bd76f8bc24fb0414c02bb8b
anor32 <burgchiken@gmail.com>
2025-11-29 22:54:32 +0300

    pep 8

--- ai_generator.py
+++ ai_generator.py
@@ -3,7 +3,7 @@ url = "https://www.warhammer-community.com/en-gb/articles/ncmflumo/the-warhammer
 
 class AimlBots:
     model = "tngtech/deepseek-r1t2-chimera:free"
-    def generate_news(self,prompt) -> str:
+    def generate_news(self,prompt:str) -> str:
         """отправляет запрос нейросети принимает пользовательский промпт возвращает ответ от бота"""
         api_key = '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'
         client = OpenAI(
