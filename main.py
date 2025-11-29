6e9f85bc7276b3e372a137e2351394ca88e8c086
anor32 <burgchiken@gmail.com>
2025-11-29 21:14:29 +0300

    pep 8

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import asyncio
-
-
 from tg_bot import NewsMakeBot
 from asyncio import create_task
 
 
 
 
-
-
 class BotCore:
-
     news_bot = NewsMakeBot()
-
-
-
     async def main(self):
-
-        monitoring = create_task(self.news_bot.news_monitoring())
+        """Запускает функции мониторинга и бота"""
+        monitoring = create_task(self.news_bot.news_monitoring(interval=120*60))
         bot_on = create_task(self.news_bot.bot.polling())
         await asyncio.gather(monitoring, bot_on)
 
