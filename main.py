7341b59bf824c3714e6d999f2853ec830de35688
anor32 <burgchiken@gmail.com>
2025-11-29 19:08:03 +0300

    архитектура проекта изменена перенесены функции из main в бота

--- main.py
+++ main.py
@@ -1,43 +1,27 @@
 import asyncio
-from time import sleep
-from gemini_api import GeminiNews
-from parcers import NewsParser
+
+
 from tg_bot import NewsMakeBot
 from asyncio import create_task
 
-np = NewsParser()
 
-gemini = GeminiNews()
+
+
 
 
 class BotCore:
-    interval = 120
-    url = ''
+
     news_bot = NewsMakeBot()
 
-    async def news_monitoring(self):
-        while True:
-            news_url = np.parce()
-            if self.url != news_url:
-                self.url = news_url
-                await self.prepare_news(news_url)
-            await asyncio.sleep(self.interval)
 
-    async def prepare_news(self, url=None):
-        if url:
-            gemini_prompt = f"""{url} на основе этой ссылки сделай новость для телеграмма добавь хештеги используй 
-                   html теги для красивой обработки текста """
-
-            news_text = gemini.make_news_with_gemini(gemini_prompt)
-            self.news_bot.send_news(news_text)
 
     async def main(self):
 
-        monitoring = create_task(self.news_monitoring())
-        bot_on = create_task(self.news_bot.bot.polling(none_stop=True))
+        monitoring = create_task(self.news_bot.news_monitoring())
+        bot_on = create_task(self.news_bot.bot.polling())
         await asyncio.gather(monitoring, bot_on)
 
 
 if __name__ == '__main__':
     bc = BotCore()
-    bc.main()
+    asyncio.run(bc.main())
