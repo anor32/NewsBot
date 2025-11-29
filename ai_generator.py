8d3a00f1e2217038049b002d0372a1d718f65292
anor32 <burgchiken@gmail.com>
2025-11-29 20:48:02 +0300

    изменена модель нейросети

--- ai_generator.py
+++ ai_generator.py
@@ -1,11 +1,10 @@
-import os
-
 from openai import OpenAI
 url = "https://www.warhammer-community.com/en-gb/articles/ncmflumo/the-warhammer-community-team-paint-the-heroes-of-warhammer-quest-darkwater/"
 
 class AimlBots:
-    def generate_news(self,prompt):
-
+    model = "tngtech/deepseek-r1t2-chimera:free"
+    def generate_news(self,prompt) -> str:
+        """отправляет запрос нейросети принимает пользовательский промпт возвращает ответ от бота"""
         api_key = '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'
         client = OpenAI(
             base_url="https://openrouter.ai/api/v1",
@@ -14,7 +13,7 @@ class AimlBots:
 
 
         response = client.chat.completions.create(
-            model="x-ai/grok-4.1-fast:free",
+            model=self.model,
             messages=[
                 {
                     "role": "user",
