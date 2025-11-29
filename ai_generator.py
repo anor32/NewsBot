62f73bc9b496a98c0d690e4e4438595781543280
anor32 <burgchiken@gmail.com>
2025-11-30 01:19:31 +0300

    обновлена модель

--- ai_generator.py
+++ ai_generator.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 url = "https://www.warhammer-community.com/en-gb/articles/ncmflumo/the-warhammer-community-team-paint-the-heroes-of-warhammer-quest-darkwater/"
 
 class AimlBots:
-    model = "tngtech/deepseek-r1t2-chimera:free"
+    model = "x-ai/grok-4.1-fast:free"
     def generate_news(self,prompt:str) -> str:
         """отправляет запрос нейросети принимает пользовательский промпт возвращает ответ от бота"""
         api_key = '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'
